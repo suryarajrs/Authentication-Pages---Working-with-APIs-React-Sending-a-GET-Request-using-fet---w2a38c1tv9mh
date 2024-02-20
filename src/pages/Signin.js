7fc6cf8d2be39559064a93cf14ea8db502e542fa
suryarajrs <suryars@outlook.in>
2024-02-21 01:16:10 +0530

    test1

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { signin } from '../api/auth';
 
 
 export const Signin = ()=>{
+
+    const navigate = useNavigate();
+
+    const [userdetails, setuserdetails]=useState({email:''
+        
+        , password:''})
+
+    const [error, setError] = useState('');    
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          await signin({ email, password });
+          navigate("/Home");
+          
+        } catch (error) {
+          setError('Invalid Data');
+        }
+      };
+
+
     return(
         <div id="signin-page">
             <h1>Sign in page</h1>
+            <form onSubmit={handleSubmit} >
+                <label htmlFor='email'>email</label>
+                <input id='email' value={userdetails.email} onChange={(e)=>{setuserdetails({...userdetails , email:e.target.value})}} type='email' required></input>
+                <label htmlFor='password'>password</label>
+                <input id='password' value={userdetails.password} onChange={(e)=>{setuserdetails({...userdetails , password:e.target.value})}} type='password' required></input>
+                <button type='submit'>Signin</button>
+                {error && <p className="error-txt">{error}</p>}
+
+            </form>
 
         </div>
     )
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { signup } from '../api/auth';
 export const Signup = () =>{
+
+    const [userdetails, setuserdetails] = useState({email:''
+        
+    , password:''})
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          await signup({ email, password });
+          navigate("/Home");
+          
+        } catch (error) {
+          setError('Invalid Data');
+        }
+      };
     return(
         <div id="signin-page">
             <h1>Sign Up page</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='email'>email</label>
+                <input id='email' value={userdetails.email}  onChange={(e)=>{setuserdetails({...userdetails , email:e.target.value})}} type='email' required></input>
+                <label htmlFor='password'>password</label>
+                <input id='password' value={userdetails.password} onChange={(e)=>{setuserdetails({...userdetails , password:e.target.value})}} type='password' required></input>
+                <button type='submit' onClick={handleClick} >Signin</button>
+
+            </form>
 
         </div>
     )
